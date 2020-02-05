fc12b9b3387255caeed68632c19fa93be1fd0fe5
S Sellami <s.sellami@innopolis.university>
2020-02-05 11:56:05 +0300

    new bag file added

--- pendubot/bldc_stand/bldc/src/bug_files_work_with.py
+++ pendubot/bldc_stand/bldc/src/bug_files_work_with.py
@@ -48,10 +48,10 @@ if __name__ == '__main__':
     rospy.init_node('rotate', anonymous=True)
     # Enter the parameters
 
-    v = 0.3  # frequency in sec^-1
-    amp = 1.5  # Amplitude
+    v = 1.0  # frequency in sec^-1
+    amp = 15  # Amplitude
     name = 'position_mult_f_A_'+str(amp)+'_v_' + str(v)+'.bag'
-    # name = 'current_A_'+str(amp)+'_v_' + str(v)+'.bag'
+    # name = 'current_mult_f_A_'+str(amp)+'_v_' + str(v)+'.bag'
     time_exec = 20
     rate = rospy.Rate(500)  # 500hz
 
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 
         while time_loop-start_time < time_exec:
             #pos = amp*math.sin(2*math.pi*v*(time_loop-start_time))
-            pos = amp*math.sin(2*math.pi*v*(time_loop-start_time)) + amp*math.sin(2*math.pi*(2*v)*(time_loop-start_time))  + amp*math.sin(2*math.pi*(3*v)*(time_loop-start_time)) + amp*math.sin(2*math.pi*(4*v)*(time_loop-start_time))
+            pos = amp*math.sin(2*math.pi*v*(time_loop-start_time)) + amp*math.sin(2*math.pi*(2*v)*(time_loop-start_time))/2  + 0*amp*math.sin(2*math.pi*(3*v)*(time_loop-start_time))/3 + 0*amp*math.sin(2*math.pi*(4*v)*(time_loop-start_time))/4
             pub.publish(pos)
             time_loop = time.time()
             rate.sleep()
