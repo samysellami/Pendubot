9ec7b117416731fd12bf6ee99f25c804a029afcc
sami <s.sellami@innopolis.university>
2020-02-12 09:14:46 +0300

    code update

--- pndbt/src/pndbt_command_python.py
+++ pndbt/src/pndbt_command_python.py
@@ -21,54 +21,55 @@ from control.matlab import *
 class pndbt():
     """docstring for ClassName"""
     def __init__(self):
-        params = {'m1': 1.085, 'm2': 0.26, 'l1': 0.25, 'l2': 0.25, 'I1': 0.008, 'I2': 0.002, 'l_com1': 0.043, 'l_com2': 0.095} 
-        theta1 = params['m1'] * (params['l_com1']**2) +  params['m2'] * (params['l1']**2) +  params['I1']
-        theta2 = params['m2'] * (params['l_com2']**2) +  params['I2']
-        theta3 = params['m2'] * params['l1'] * params['l_com2'] 
-        theta4 = params['m1'] * params['l_com1'] +  params['m2'] * params['l1']
-        theta5 = params['m2'] * params['l_com2']
-        self.g = 9.81
-        self.theta = np.array([theta1, theta2, theta3, theta4, theta5])  
+      params = {'m1': 1.085, 'm2': 0.26, 'l1': 0.25, 'l2': 0.25, 'I1': 0.008, 'I2': 0.002, 'l_com1': 0.043, 'l_com2': 0.095} 
+      theta1 = params['m1'] * (params['l_com1']**2) +  params['m2'] * (params['l1']**2) +  params['I1']
+      theta2 = params['m2'] * (params['l_com2']**2) +  params['I2']
+      theta3 = params['m2'] * params['l1'] * params['l_com2'] 
+      theta4 = params['m1'] * params['l_com1'] +  params['m2'] * params['l1']
+      theta5 = params['m2'] * params['l_com2']
+      self.g = 9.81
+      self.theta = np.array([theta1, theta2, theta3, theta4, theta5])  
          
         
     def D_mtrx(self, q):
-        D = np.zeros((2,2))
-        D[0,0] =  self.theta[0] + self.theta[1] + 2 * self.theta[3] * math.cos(q[1])
-        D[0,1] =  self.theta[1] + self.theta[2] * math.cos(q[1])
-        D[1,0] =  self.theta[1] + self.theta[2] * math.cos(q[1])
-        D[1,1] =  self.theta[2]
-        return D 
+      D = np.zeros((2,2))
+      D[0,0] =  self.theta[0] + self.theta[1] + 2 * self.theta[3] * math.cos(q[1])
+      D[0,1] =  self.theta[1] + self.theta[2] * math.cos(q[1])
+      D[1,0] =  self.theta[1] + self.theta[2] * math.cos(q[1])
+      D[1,1] =  self.theta[2]
+      return D 
         
     def C_mtrx(self, q, q_d):
-        C = np.zeros((2,2))
-        C[0,0] =  -self.theta[2] * math.sin(q[1]) * q_d[1]  
-        C[0,1] =  -self.theta[2] * math.sin(q[1]) * q_d[1] -self.theta[2] * math.sin(q[1]) * q_d[0]
-        C[1,0] =  self.theta[2] * math.sin(q[1]) * q_d[0]
-        C[1,1] =  0
-        return C 
+      C = np.zeros((2,2))
+      C[0,0] =  -self.theta[2] * math.sin(q[1]) * q_d[1]  
+      C[0,1] =  -self.theta[2] * math.sin(q[1]) * q_d[1] -self.theta[2] * math.sin(q[1]) * q_d[0]
+      C[1,0] =  self.theta[2] * math.sin(q[1]) * q_d[0]
+      C[1,1] =  0
+      return C 
 
     def g_vctr(self, q):
-        g = np.zeros(2)
-        g[0] =  self.theta[3] * self.g * math.cos(q[0]) + self.theta[4] * self.g * math.cos(q[0] + q[1])   
-        g[1] =  self.theta[4] * self.g * math.cos(q[0] + q[1])
-        return g 
+      g = np.zeros(2)
+      g[0] =  self.theta[3] * self.g * math.cos(q[0]) + self.theta[4] * self.g * math.cos(q[0] + q[1])   
+      g[1] =  self.theta[4] * self.g * math.cos(q[0] + q[1])
+      return g 
 
 
     def A_lin(self):
-	C = np.zeros((4,4))	
-	C[0,2] = 1
-	C[1,3] = 1
- 	C[2,0] = -(self.g * self.theta[1] * self.theta[3] - self.g * self.theta[2] * self.theta[4]) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
-	C[2,1] = (self.g * self.theta[2] * self.theta[4]) / (-  self.theta[2]**2 + self.theta[0] * self.theta[1])
-        C[3,0] =  (self.g * self.theta[0] * self.theta[4] + self.g * self.theta[1] * self.theta[3] - self.g * self.theta[2] * self.theta[3] - self.g * self.theta[2] 			* self.theta[4]) / (- self.theta[2] **2 + self.theta[0] * self.theta[1])
-	C[3,1] = (self.g * self.theta[0] * self.theta[4] - self.g * self.theta[2] * self.theta[4]) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
-        return C 
+      A = np.zeros((4,4))	
+      A[0,2] = 1
+      A[1,3] = 1
+      A[2,0] = -(self.g * self.theta[1] * self.theta[3] - self.g * self.theta[2] * self.theta[4]) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
+      A[2,1] = (self.g * self.theta[2] * self.theta[4]) / (-  self.theta[2]**2 + self.theta[0] * self.theta[1])
+      A[3,0] =  (self.g * self.theta[0] * self.theta[4] + self.g * self.theta[1] * self.theta[3] - self.g * self.theta[2] * self.theta[3]  \
+	- self.g * self.theta[2]* self.theta[4]) / (- self.theta[2] **2 + self.theta[0] * self.theta[1])
+      A[3,1] = (self.g * self.theta[0] * self.theta[4] - self.g * self.theta[2] * self.theta[4]) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
+      return A 
 
     def B_lin(self):
-	B = np.zeros((4,1))
-	B[1,0] = self.theta[1] / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
- 	B[3,0] = -( self.theta[1]  -  self.theta[2] ) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
-        return B 
+      B = np.zeros((4,1))
+      B[1,0] = self.theta[1] / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
+      B[3,0] = -( self.theta[1]  -  self.theta[2] ) / (- self.theta[2] ** 2 + self.theta[0] * self.theta[1])
+      return B 
 
 if __name__ == '__main__':
 
@@ -77,12 +78,13 @@ if __name__ == '__main__':
     q_d  = np.zeros(2)
 
     pendubot  = pndbt()
-    print(pendubot.B_lin())
+    #print(pendubot.g_vctr())
+    
     Q = np.zeros((4, 4))
     Q[0,0] = 10
     Q[1,1] = 10
     R = 0.1		
-    K, S, E = control.lqr(self.A_lin(), self.B_lin(), Q, R)
+    K, S, E = control.lqr(pendubot.A_lin(), pendubot.B_lin(), Q, R)
 
     
     
